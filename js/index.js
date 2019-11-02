71297f64408c945d3aff42508a892c6c90e51898
Ben Urena <urenadev@gmail.com>
2019-11-02 02:47:39 -0500

    added 3rd event listener

--- js/index.js
+++ js/index.js
@@ -4,14 +4,23 @@
 const Btn1 = document.querySelector('.btn');
 
 Btn1.addEventListener('click', (event) => {
-    event.target.style.background= 'hotpink';
-  })
+  event.target.style.background= 'hotpink';
+})
 
 //2. On Hover make the navigator links purple
-  const links = document.querySelectorAll('.nav-link');
-  links.forEach(link =>{
-      link.addEventListener('mouseover', (element) =>{
+const links = document.querySelectorAll('.nav-link');
+
+links.forEach(link => {
+  link.addEventListener('mouseover', (element) => {
     element.target.style.color = 'purple';
     element.preventDefault();
-})})
+  })
+})
 
+//3. On Double click, change text on button 2
+
+const Btn2 = document.querySelectorAll('.btn')[1];
+
+Btn2.addEventListener('dblclick', () => {
+  Btn2.textContent = 'CHAAAAAANGE!';
+})
