8b1c698513cd0387bb1250c0422b1a4c620ce0aa
Ben Urena <urenadev@gmail.com>
2019-11-02 02:28:42 -0500

    added 1 event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+//1. on click make button 1 hot pink
+const Btn1 = document.querySelector('.btn');
+
+Btn1.addEventListener('click', (event) => {
+    event.target.style.background= 'hotpink';
+  })
