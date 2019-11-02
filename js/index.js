211f0885c39e83d3facc543a064e64fd2c8bf370
Ben Urena <urenadev@gmail.com>
2019-11-02 02:37:33 -0500

    added 2nd event listener

--- js/index.js
+++ js/index.js
@@ -6,3 +6,12 @@ const Btn1 = document.querySelector('.btn');
 Btn1.addEventListener('click', (event) => {
     event.target.style.background= 'hotpink';
   })
+
+//2. On Hover make the navigator links purple
+  const links = document.querySelectorAll('.nav-link');
+  links.forEach(link =>{
+      link.addEventListener('mouseover', (element) =>{
+    element.target.style.color = 'purple';
+    element.preventDefault();
+})})
+
